d9e6efbccde70fec511e4c6e942186c425ccc0cf
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-06-09 13:17:20 +0200

    First build setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: 'src/main.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
